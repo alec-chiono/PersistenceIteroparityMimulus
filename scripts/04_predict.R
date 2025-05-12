b046d91f5815822890f3b527b58a70fcd5d8c0b4
Alec Chiono <alec.chiono@colorado.edu>
2025-05-12 15:33:33 -0600

    Remove trailing white spaces

--- scripts/04_predict.R
+++ scripts/04_predict.R
@@ -7,7 +7,7 @@ librarian::shelf(tidybayes)
 persistence_fit <- readRDS("output/persistence_fit.RDS")
 
 ## Marginal predictions (i.e. ignoring random effects)
-persistence_marg_pred <- germ_df %>% 
+persistence_marg_pred <- germ_df %>%
   modelr::data_grid(ecotype, cold_stratification, water_potential) %>% #create data.frame with all combinations of fixed effects
   epred_draws(persistence_fit, ., re_formula=~0) %>% ungroup() %>% #make predictions
   rename(p=.epred) # #rename prediction variable
@@ -22,12 +22,12 @@ persistence_cond_pred_from <- germ_df %>% # create data.frame to predict from
     plate_pair=as.factor(unname(sapply(as.character(plate), function(x) strsplit(x,"_")[[1]][1]))) #create plate_pair column from information in plate
   )
 
-for (i in 1:length(levels(germ_df$site))) { #predict for each site separately 
+for (i in 1:length(levels(germ_df$site))) { #predict for each site separately
   persistence_cond_pred_list[[i]] <- persistence_cond_pred_from %>% #because otherwise memory
     filter(site==levels(germ_df$site)[i]) %>% #limit is reached
-    epred_draws(persistence_fit, .) %>% ungroup() %>% 
-    rename(p=.epred) %>% 
-    group_by(ecotype, site, seed_family, cold_stratification, water_potential, .draw) %>% 
+    epred_draws(persistence_fit, .) %>% ungroup() %>%
+    rename(p=.epred) %>%
+    group_by(ecotype, site, seed_family, cold_stratification, water_potential, .draw) %>%
     summarize(p=mean(p), .groups="drop") #average over plate-pairs/plates to remove effect of location
 }
 
@@ -39,7 +39,7 @@ rm(persistence_cond_pred_list, persistence_cond_pred_from) #remove intermediate 
 viability_fit <- readRDS("output/viability_fit.RDS")
 
 ## Marginal predictions (i.e. ignoring random effects)
-viability_pred <- germ_df %>% 
+viability_pred <- germ_df %>%
   modelr::data_grid(ecotype, cold_stratification, water_potential) %>% #create data.frame with all combinations of fixed effects
   epred_draws(viability_fit, ., re_formula=~0) %>% ungroup() %>% #make predictions
   rename(p=.epred) #rename prediction variable
@@ -48,8 +48,8 @@ viability_pred <- germ_df %>%
 time_fit <- readRDS("output/time_fit.RDS")
 
 ## Marginal predictions (i.e. ignoring random effects)
-time_pred <- time_df %>% 
+time_pred <- time_df %>%
   modelr::data_grid(ecotype, cold_stratification, water_potential) %>% #create data.frame with all combinations of fixed effects
   epred_draws(time_fit, ., re_formula=~0) %>% ungroup() %>% #make predictions
-  mutate(.epred=1/.epred)  %>% #convert proportion to expected day of germination 
+  mutate(.epred=1/.epred)  %>% #convert proportion to expected day of germination
   rename(Day=.epred) #rename prediction variable
